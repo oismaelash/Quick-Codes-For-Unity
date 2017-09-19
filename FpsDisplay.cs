a4a5a0dad4681d8df0c2a44cf35dd77db94a326d
Ismael Nascimento <ismaeln.business@gmail.com>
2017-09-19 08:56:46 -0300

    Create FpsDisplay.cs

---
+++ FpsDisplay.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+/*
+ * Set this script in any GameObject on scene for active.
+ */
+
+public class FpsDisplay : MonoBehaviour
+{
+    float deltaTime = 0.0f;
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI()
+    {
+        int width = Screen.width, height = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, width, height * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = height * 2 / 60;
+        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        GUI.Label(rect, text, style);
+    }
+}
