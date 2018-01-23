3e14fb220b6442a75fc79db682bd6741214b6e85
Ismael Nascimento <contato@ismaelnascimento.com>
2018-01-23 17:54:32 -0200

    Create LaunchAppExternal.cs

---
+++ LaunchAppExternal.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class LaunchAppExternal : MonoBehaviour
+{
+    public void LaunchApp(string bundleId)
+    {
+        bool fail = false;
+        AndroidJavaClass up = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ca = up.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject packageManager = ca.Call<AndroidJavaObject>("getPackageManager");
+        AndroidJavaObject launchIntent = null;
+
+        try
+        {
+            launchIntent = packageManager.Call<AndroidJavaObject>("getLaunchIntentForPackage", bundleId);
+        }
+        catch (System.Exception e)
+        {
+            fail = true;
+        }
+
+        if (fail) // open in browser
+            print("Fail open app");
+        else //open the app
+            ca.Call("startActivity", launchIntent);
+
+        up.Dispose();
+        ca.Dispose();
+        packageManager.Dispose();
+        launchIntent.Dispose();
+    }
+}
