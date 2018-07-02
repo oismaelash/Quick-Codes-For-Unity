ae1e6d84ff8a0646a75b2c3e775b6004f5e4f290
Ismael Nascimento <contato@ismaelnascimento.com>
2018-07-01 23:28:43 -0300

    With Action

--- FadeAlphaGraphicUI.cs
+++ FadeAlphaGraphicUI.cs
@@ -1,19 +1,28 @@
 public static class FadeAlphaGraphicUI
 {
-    public static void CrossFadeAlphaFixed(this Graphic img, float alpha, float duration, bool ignoreTimeScale)
+    public static void CrossFadeAlphaFixed(this Graphic graphic, float alpha, float duration, Action callback)
     {
-        //Make the alpha 1
-        Color fixedColor = img.color;
-        fixedColor.a = 1;
-        img.color = fixedColor;
+        MonoBehaviour monoBehaviour = graphic.GetComponent<MonoBehaviour>();
+        monoBehaviour.StartCoroutine(CrossFadeAlphaFixed_Coroutine(graphic, alpha, duration, callback));
+    }
 
-        //Set the 0 to zero then duration to 0
-        img.CrossFadeAlpha(0f, 0f, true);
+    public static IEnumerator CrossFadeAlphaFixed_Coroutine(Graphic img, float alpha, float duration, Action callback)
+    {
+        Color currentColor = img.color;
+        Color visibleColor = img.color;
+        visibleColor.a = alpha;
+        float counter = 0;
 
-        //Finally perform CrossFadeAlpha
-        img.CrossFadeAlpha(alpha, duration, ignoreTimeScale);
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            img.color = Color.Lerp(currentColor, visibleColor, counter / duration);
+            yield return null;
+        }
+
+        if (callback != null) callback();
     }
     
     // How Use, e.g
-    // Image.CrossFadeAlphaFixed(1f, 1f. false);
+    // Image.CrossFadeAlphaFixed(1f, 1f. false, () => print("Finished"));
 }
